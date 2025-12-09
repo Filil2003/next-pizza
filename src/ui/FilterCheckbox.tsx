eece1e303dbd385adb37d9aa1b2dfadf134ccdba
Ilya <filil2003@yandex.ru>
2025-12-09 16:32:13 +0300

    feat: add FilterCheckbox component

---
+++ src/ui/FilterCheckbox.tsx
@@ -0,0 +1,26 @@
+import { useId } from "react";
+import { Checkbox } from "#/ui/Checkbox.tsx";
+
+/* ===== Typing props ===== */
+interface Props {
+  label: string;
+  value: string;
+  onCheckedChange?: (checked: boolean) => void;
+  checked?: boolean;
+}
+
+/* ===== FilterCheckbox component ===== */
+export function FilterCheckbox({ label, ...restProps }: Props) {
+  const checkboxId = useId();
+  return (
+    <div className="flex items-center space-x-2">
+      <Checkbox className="rounded-lg w-6 h-6" id={checkboxId} {...restProps} />
+      <label
+        className="leading-none cursor-pointer flex-1"
+        htmlFor={checkboxId}
+      >
+        {label}
+      </label>
+    </div>
+  );
+}
