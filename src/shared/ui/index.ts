cfde5bc078437f371d23ab09558629599677b6d2
Ilya <filil2003@yandex.ru>
2026-01-04 21:21:14 +0300

    feat(ui): add Dialog component

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,6 +1,8 @@
 export { Button } from "./Button";
 export { Checkbox } from "./Checkbox";
 export { Container } from "./Container";
+export * from "./Dialog";
 export { Heading } from "./Heading";
 export { Input } from "./Input";
 export { RangeSlider } from "./RangeSlider";
+export { ToggleGroup } from "./ToggleGroup";
