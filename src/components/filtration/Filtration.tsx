a819d787b889c7da5aa0c0e802f728d6a8eb47f5
Ilya <filil2003@yandex.ru>
2026-01-20 21:21:20 +0300

    fix(filter): add Suspense wrapper

--- src/components/filtration/Filtration.tsx
+++ src/components/filtration/Filtration.tsx
@@ -1,5 +1,6 @@
 "use server";
 
+import { Suspense } from "react";
 import { zen } from "#/shared/lib/zenstack";
 import { IngredientsFilter } from "./IngredientsFilter.tsx";
 import { PriceFilter } from "./PriceFilter.tsx";
@@ -22,15 +23,19 @@ export async function Filtration({ className }: Props) {
     <aside className={className}>
       <h4 className="text-2xl mb-5 font-bold">Фильтрация</h4>
 
-      <PriceFilter />
+      <Suspense>
+        <PriceFilter />
+      </Suspense>
 
-      <IngredientsFilter
-        className="mt-5 border-y border-y-neutral-100 py-6"
-        ingredients={ingredientsRow.map(({ name, id }) => ({
-          label: name,
-          value: id
-        }))}
-      />
+      <Suspense>
+        <IngredientsFilter
+          className="mt-5 border-y border-y-neutral-100 py-6"
+          ingredients={ingredientsRow.map(({ name, id }) => ({
+            label: name,
+            value: id
+          }))}
+        />
+      </Suspense>
     </aside>
   );
 }
